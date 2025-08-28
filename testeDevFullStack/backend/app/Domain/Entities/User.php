0035c6a750ef263e1dc793b4f96475671cc776b6
Kiyoshi Takahama <k1y05h17k@gmail.com>
2025-08-27 21:28:20 -0300

    feat(Entities/User) update code

--- testeDevFullStack/backend/app/Domain/Entities/User.php
+++ testeDevFullStack/backend/app/Domain/Entities/User.php
@@ -1,28 +1,38 @@
 <?php
 
-
 namespace App\Domain\Entities;
 
 use App\Domain\ValueObjects\RoleLevel;
 
-
-class User
+final class User
 {
     public function __construct(
-        public readonly string $id,
+        public ?int $id,
         public string $name,
         public string $email,
         public string $passwordHash,
         public RoleLevel $roleLevel,
-        public readonly \DateTimeImmutable $createdAt,
-        public \DateTimeImmutable $updatedAt,
-    ) {
-        // Invariantes básicas
-        if (!filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
-            throw new \InvalidArgumentException('Invalid email');
-        }
-        if (!$this->roleLevel->isValid()) {
-            throw new \InvalidArgumentException('Invalid role level');
-        }
+        public ?\DateTimeImmutable $createdAt = null,
+        public ?\DateTimeImmutable $updatedAt = null,
+    ) {}
+
+    public static function new(string $name, string $email, string $passwordHash, RoleLevel $roleLevel): self
+    {
+        return new self(null, $name, $email, $passwordHash, $roleLevel);
+    }
+
+    public function changeRole(RoleLevel $newRole): void
+    {
+        $this->roleLevel = $newRole;
+    }
+
+    public function rename(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function rehashPassword(string $passwordHash): void
+    {
+        $this->passwordHash = $passwordHash;
     }
 }
