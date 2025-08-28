e4b0be69dfd64173dd785defdf21e7a5b8fe0f9a
Kiyoshi Takahama <k1y05h17k@gmail.com>
2025-08-28 12:49:31 -0300

    chore(container): registra binding de UserRepository no AppServiceProvider

--- testeDevFullStack/backend/app/Providers/AppServiceProvider.php
+++ testeDevFullStack/backend/app/Providers/AppServiceProvider.php
@@ -3,20 +3,19 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Repositories\UserRepositoryInterface;
+use App\Repositories\InMemoryUserRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        //
+        $this->app->bind(
+            UserRepositoryInterface::class,
+            InMemoryUserRepository::class
+        );
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         //
